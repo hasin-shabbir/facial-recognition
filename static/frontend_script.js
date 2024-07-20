f298033081aedf20fb1c170f38c7b4ab0cc9b5a9
hasin-shabbir <mhs581@nyu.edu>
2024-07-20 20:56:35 +0500

    fix weird window location string issue

--- static/frontend_script.js
+++ static/frontend_script.js
@@ -15,7 +15,7 @@ function initializePageText(mode){
     }
 }
 function initializePage(mode, username){
-    const baseUrl = (window.location.origin !== undefined) && (window.location.origin !== null) && (window.location.origin !== "null") ? window.location.origin : 'localhost:8003';
+    const baseUrl = (window.location.origin !== undefined) && (window.location.origin !== null) && (window.location.origin !== "null") ? window.location.origin.replace('https//','https://') : 'localhost:8003';
     const socketUrl = mode === 'register' ? `wss://${baseUrl}/register` : `wss://${baseUrl}/login`;
     const socket = new WebSocket(socketUrl);
     const constraints = { video: true };
